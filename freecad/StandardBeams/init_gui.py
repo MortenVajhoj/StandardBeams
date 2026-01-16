a5f323c77c16fb9d4d2f16fab305ea9311cdb4b2
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-16 01:22:33 -0500

    Add SPDX annotations

--- freecad/StandardBeams/init_gui.py
+++ freecad/StandardBeams/init_gui.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the Standard Beams addon.
+
+
 import FreeCADGui as Gui
 import FreeCAD
 import os
--- freecad/StandardBeams/scripts/__init__.py
+++ freecad/StandardBeams/scripts/__init__.py
@@ -1,1 +1,2 @@
-# Standard Beams Scripts Package
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-FileNotice: Part of the Standard Beams addon.
